15fa7b42ab972587a98c5d5d60920c4465bb91a6
David Barleta <davidjoshuabarleta@gmail.com>
2026-04-15 17:42:10 +0800

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,40 @@
+/* This SQL script creates a new database named 'OlistDataWarehouse', dropping and recreating it
+if it already exists. The database created contains three layers/schemas: bronze, silver,
+and gold, following a medallion architecture.
+*/
+
+USE master;
+GO
+
+IF EXISTS (
+	SELECT name
+	FROM sys.databases
+	WHERE name = 'OlistDataWarehouse'
+)
+BEGIN
+	ALTER DATABASE OlistDataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE OlistDataWarehouse
+END;
+GO
+
+CREATE DATABASE OlistDataWarehouse;
+GO
+
+USE OlistDataWarehouse;
+GO
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
+
+-- Check if the created schemas exist
+
+SELECT SCHEMA_NAME
+FROM INFORMATION_SCHEMA.SCHEMATA
+WHERE SCHEMA_NAME IN ('bronze', 'silver', 'gold');
+GO
